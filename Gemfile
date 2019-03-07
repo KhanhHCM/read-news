60bc23246b602c08b51709131346415fc78f0fe3
khanhhcm <khanh.nvhcm@gmail.com>
2019-03-07 16:35:05 +0700

    commit

--- Gemfile
+++ Gemfile
@@ -43,6 +43,11 @@ gem 'nokogiri', '~> 1.6', '>= 1.6.8'
 gem "ruby-readability", :require => 'readability'
 gem 'fastimage'
 
+#bootsnap
+gem 'bootstrap', '~> 4.3.1'
+
+gem 'jquery-rails'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -1,2 +1,21 @@
 class NewsController < ApplicationController
+  def index
+    doc = Nokogiri::HTML(open('https://news.ycombinator.com/best')).css('a.storylink')
+    @list_news = []
+    doc.each do |post|
+      news = {}
+      link = post['href'].include?('http') ? post['href'] : "https://news.ycombinator.com/#{post['href']}"
+      source = Nokogiri::HTML(open(link))
+      description = source.xpath('//meta[@name="description"]')
+      image_url   = source.xpath('//meta[@name="image"]')
+      news["description"] = description.first ? description.first['content'] : post.text
+      news["image_url"]   = image_url.first ? image_url.first['content'] : "default.png"
+      news["url"] = link
+      news["title"] = post.text
+      @list_news << news
+    end
+  end
+
+  def show
+  end
 end
