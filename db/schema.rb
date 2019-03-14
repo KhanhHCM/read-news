34a5f29cd7bbfc10f11007fb906d82aa7bde78aa
khanhhcm <khanh.nvhcm@gmail.com>
2019-03-14 10:38:35 +0700

    change db to postgress

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_03_13_093510) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "authors", force: :cascade do |t|
     t.string "name", null: false
     t.datetime "created_at", null: false
@@ -25,9 +28,10 @@ ActiveRecord::Schema.define(version: 2019_03_13_093510) do
     t.string "link_image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "author_id"
+    t.bigint "author_id"
     t.string "slug"
     t.index ["author_id"], name: "index_posts_on_author_id"
   end
 
+  add_foreign_key "posts", "authors"
 end
