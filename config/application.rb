48d6e23a615e9b653b38ba61041213af2a3edab9
khanhhcm <khanh.nvhcm@gmail.com>
2019-03-14 11:30:27 +0700

    font fix

--- config/application.rb
+++ config/application.rb
@@ -17,5 +17,6 @@ module ReaderNews
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
   end
 end
