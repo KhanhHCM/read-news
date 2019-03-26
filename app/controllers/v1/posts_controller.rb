fd01a14faf45c90ca3eef50d9883f63bba6c4927
khanhhcm <khanh.nvhcm@gmail.com>
2019-03-26 10:00:05 +0700

    show

--- app/controllers/v1/posts_controller.rb
+++ app/controllers/v1/posts_controller.rb
@@ -4,4 +4,13 @@ class V1::PostsController < V1::BaseController
 
     render json: posts, root: false
   end
+
+  def show
+    post = Post.find(params[:id])
+
+    render json: { news_head: post,
+                   news_body: post.news_content}, root: false
+
+  end
+
 end
