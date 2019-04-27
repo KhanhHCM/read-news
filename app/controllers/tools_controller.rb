16ee733e8814a460bebde2ea2cca1dc7b8bc0994
khanhhcm <khanh.nvhcm@gmail.com>
2019-04-27 17:54:37 +0700

    fix layout

--- app/controllers/tools_controller.rb
+++ app/controllers/tools_controller.rb
@@ -1,39 +1,31 @@
 class ToolsController < ApplicationController
 
   def new
-  end
-
-  def estimate
-  end
-
-  def sum(consum)
-    price1 = 1678
-    price2 = 1734
-    price3 = 2014
-    price4 = 2536
-    price5 = 2834
-    price6 = 2927
-
-    lv1 = price1*50
-    lv2 = lv1 + price2*50
-    lv3 = lv2 + price3*100
-    lv4 = lv3 + price4*100
-    lv5 = lv4 + price5*100
-
-    case consum
-    when 1..50
-      consum * price1
-    when 51..100
-      lv1 + (consum - 50)*price2
-    when 100..200
-      lv2 + (consum - 100)*price3
-    when 200..300
-      lv3 + (consum - 200)*price4
-    when 300..400
-      lv4 + (consum - 300)*price5
-    else
-      lv5 + (consum - 400)*price6
+    new_price = [1678, 1734, 2014, 2536, 2834, 2927]
+    old_price = [1549, 1600, 1858, 2340, 2615, 2710]
+    lv = ["0~50", "51~100", "101~200", "201~300", "301~400", "401~"]
+    per_up = []
+    t = 0
+    new_price.each do |n|
+      per_up << percent_up(n, old_price[t])
+      t+= 1
     end
+
+    @data = []
+    (0..5).each do |n|
+      @data << {
+        new_price: new_price[n],
+        old_price: old_price[n],
+        lv:        lv[n],
+        per_up:    per_up[n],
+        rank:      n
+      }
+    end
+  end
+
+  private
+  def percent_up(new_price, old_price)
+    ((new_price - old_price).fdiv old_price)*100
   end
 
 end
